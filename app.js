10bb077d0e2ff550ce3e177b982ec2733e1a0c76
smwbtech <smwb.tech@gmail.com>
2017-10-15 23:10:18 +0300

    Тестовая версия готова. Необходимо сделать проверку готовности этикеток.

--- app.js
+++ app.js
@@ -2,12 +2,8 @@ const   express = require('express'),
         multer = require('multer'),
         fs = require('fs'),
         xlsx2json = require('xlsx2json'),
-        jsBarcode = require('jsbarcode'),
-        canvas = require('canvas'),
-        gm = require('gm'),
-        im = require('gm').subClass({imageMagick: true}),
-        Image = canvas.Image;
-
+        stickers = require('./lib/stickers.js').stickers(),
+        zipdir = require('zip-dir');
 
 let upload = multer({ dest: './public/upload/' });
 let app = express();
@@ -15,98 +11,46 @@ let app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'pug');
 
-function compositeSticker(obj) {
-    return new Promise((res, rej) => {
-        console.log(1);
-        gm('./public/img/pattern.jpg')
-        .composite(`./public/upload/${obj.barcode}.png`)
-        .geometry('+20+460')
-        .write('./public/img/pattern-changed.jpg', (err) => {
-            if(err) throw err;
-            res(obj);
-        });
-    });
-}
-
-function drawText(obj) {
-    console.log(1)
-    gm('./public/img/pattern-changed.jpg')
-    .font(__dirname + "/public/fonts/ArialRegular.ttf", 16)
-    .drawText(300, 39, `${obj.article}`)
-    .drawText(300, 58, `${obj.size}`)
-    .font(__dirname + "/public/fonts/ArialBold.ttf", 16)
-    .drawText(12, 100, obj.title.match(/женская/) ? 'ФУТБОЛКА ЖЕНСКАЯ' : 'ФУТБОЛКА МУЖСКАЯ' )
-    .font(__dirname + "/public/fonts/ArialItalic.ttf", 17)
-    .drawText(12, 460, `${obj.title}`)
-    .write(`./public/upload/${obj.barcode}sticker.jpg`, (err) => {
-        if(err) throw err;
-        Promise.resolve(obj);
-    });
-}
-
-function createSticker(obj) {
-    return new Promise( (res, rej) => {
-        let cnv = new canvas(400, 130);
-        let barCode = jsBarcode(cnv, `${obj.barcode}`, { format: "ean13", width: 3, font: 'sans-serif'});
-        let png = fs.createWriteStream(`./public/upload/${obj.barcode}.png`);
-        let stream = cnv.pngStream(barCode);
-
-        stream.on('data', (chunk) => {
-          png.write(chunk);
-        });
-
-        stream.on('end', () => {
-            setTimeout( ()=> res(obj), 300);
-
-        });
-    });
-}
-
 app.get('/', (req, res, err) => {
     if(err) console.error(err);
     res.render('index');
 });
 
+app.get('/readystatus', (req, res, err) => {
+    if(global.readystatus === true) {
+        res.status(200);
+        res.send('<a download href="/download/stickers.zip">Скачать архив</a>');
+    }
+    else {
+        res.status(100);
+        res.send('not ready');
+    }
+
+});
+
 app.post('/xlstojson', upload.single('tablefile'), (req, res, err) => {
     if(err) console.error(err);
-    console.log(req.file);
-    var tmp_path = req.file.path;
-    /** The original name of the uploaded file
-        stored in the variable "originalname". **/
+    let tmp_path = req.file.path;
     let target_path = './public/upload/' + req.file.originalname;
-
-    /** A better way to copy the uploaded file. **/
     let src = fs.createReadStream(tmp_path);
     let dest = fs.createWriteStream(target_path);
     src.pipe(dest);
-    fs.unlink(tmp_path); //deleting the tmp_path
-    src.on('end', function() {
-        // res.render('complete');
-        xlsx2json(target_path, {
-            mapping: {
-            'article': 'A',
-            'title': 'B',
-            'size': 'C',
-            'barcode': 'D'
-        }
-        })
-        .then( (json) => {
-            res.status(200);
-            res.send(json);
-        })
-        .catch( (err) => console.error(err));
+    fs.unlink(tmp_path);
+    src.on('end', () => {
+        console.log('res');
+        global.xlsx = target_path;
+        res.status(200);
+        res.json({link: '<a href="/stickers">Сгенерировать этикетки</a>'});
     });
     src.on('error', function(err) {
          res.render('error');
      });
-    // res.status(200);
-    // res.send('{"name":"json"}');
-
 });
 
 app.get('/stickers', (req, res, err) => {
     if(err) console.error(err);
-    xlsx2json('./public/upload/tablefile.xlsx', {
+    global.readystatus === false;
+    xlsx2json(global.xlsx, {
         dataStartingRow: 2,
         mapping: {
         'article': 'A',
@@ -115,19 +59,26 @@ app.get('/stickers', (req, res, err) => {
         'barcode': 'D'
     }})
     .then( (json) => {
-
-        // console.log(json[0][0]);
         for(let arr of json) {
             var i = 0;
-            var intervalId = setInterval( () => {
-                createSticker(arr[i])
-                .then(compositeSticker)
-                .then((obj) => drawText(obj))
+            let intervalId = setInterval( () => {
+                stickers.createSticker(arr[i])
+                .then(stickers.compositeSticker)
+                .then((obj) => stickers.drawText(obj))
                 .catch( (err) => console.error(err));
-                i < arr.length ? i++ : clearInterval(intervalId);
-            }, 300);
+                if(i < arr.length-1) i++
+                else{
+                    console.log('done');
+                    zipdir('./public/upload', { saveTo: './public/download/stickers.zip' }, (err, buffer) => {
+                        if(err) console.error(err);
+                    });
+                    global.readystatus === true;
+                    clearInterval(intervalId);
+                }
+            }, 600);
         }
-
+        res.status(200);
+        res.send('Some text here');
     })
     .catch( (err) => console.error(err));
 
@@ -152,13 +103,3 @@ app.use( (err, req, res, next) => {
 app.listen(app.get('port'), () => {
     console.log('Наш сервер работает на 3000 порту');
 });
-
-// var arr = [[1,2,3,4,5,6,7,8]];
-//
-// for (let prop of arr) {
-//     var i = 0;
-//     var intervalId = setInterval( () => {
-//         console.log(prop[i]);
-//         i < prop.length ? i++ : clearInterval(intervalId);
-//     }, 300);
-// }
--- public/js/code.js
+++ public/js/code.js
@@ -1,6 +1,8 @@
 window.onload = () => {
 
     let form = document.forms[0];
+    let flashMessage = document.getElementById('flashMessage');
+    let status = document.getElementById('status');
 
     form.addEventListener('submit', submitHandler);
 
@@ -13,12 +15,48 @@ window.onload = () => {
             if(res.status === 200) return res.json();
         })
         .then( (json) => {
-            console.log(json);
+            flashMessage.innerHTML = json.link;
+            let link = document.querySelector('#flashMessage a');
+            console.log(link);
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.target.classList.add('inactive');
+                status.classList.remove('inactive');
+                fetch(e.target.href, {method: 'get'})
+                .then( (res) =>{
+                    if(res.status === 200) {
+                        checkReady();
+                    }
+                })
+                .catch( (err) => console.log(err));
+            });
+
         })
         .catch( (err) => {
             console.log(err);
         });
 
+    }
+
+    // helper
+    function checkReady() {
+        console.log('start check');
+        fetch('/readystatus', {method: 'get'})
+        .then( (res) => {
+            if(res.status === 200) {
+                console.log('dsadsada');
+                foo();
+
+            }
+        })
+        .catch((err) => console.log(err));
+    }
+
+    function foo() {
+        status.classList.remove('active');
+        status.classList.add('inactive');
+        flashMessage.innerHTML = '<a download href="download/stickers.zip">Скачать стикеры</a>';
+        status.classList.remove('inactive');
     }
 
 }
